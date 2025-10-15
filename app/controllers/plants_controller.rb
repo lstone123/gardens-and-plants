dd5253b78238cdcd1cce7d49945e299abef2fa3e
Louise Stone <louisemstone@hotmail.com>
2025-10-15 17:17:59 +0100

    add comments

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,13 +1,12 @@
 class PlantsController < ApplicationController
   def create
-    @plant = Plant.new(plant_params)
-    @garden = Garden.find(params[:garden_id])
-    @plant.garden = @garden
-    # save the plant
-    if @plant.save
+    @plant = Plant.new(plant_params) # creating the plant using the plant hash inside the strong params
+    @garden = Garden.find(params[:garden_id]) # find the garden asocciated with this plant using the id found in the url (FROM NESTED ROUTE!)
+    @plant.garden = @garden # assigning the instance of garden found to the instance of plant
+    if @plant.save # save the plant
       redirect_to garden_path(@garden) # if it saves redirect to the garden page
     else
-      render "gardens/show", status: :unprocessable_entity  # if it doesn't save, need to render the form wiht the errors
+      render "gardens/show", status: :unprocessable_entity  # if it doesn't save, need to render the form with the errors
     end
   end
 
