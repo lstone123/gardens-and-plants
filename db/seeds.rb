ddd1e9d7c4b329770e8575c309a1665ab7e329eb
Louise Stone <louisemstone@hotmail.com>
2025-10-15 16:19:25 +0100

    seed file build

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "cleaning the development database"
+Garden.destroy_all if Rails.env.development?
+
+Garden.create!(
+  name: "My Little Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
+)
+
+Garden.create!(
+  name: "My Other Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+)
+
+puts "created #{Garden.count} gardens..."
